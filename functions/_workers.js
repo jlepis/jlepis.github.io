2b60988c04159e38c966e3b51aa56c24d7563311
Joseph Christopher Lepis <jlepis2010@gmail.com>
2024-06-27 12:58:58 -1000

    UPdated cors attempt

--- functions/_workers.js
+++ functions/_workers.js
@@ -1,8 +1,18 @@
 export async function onRequest(context) {
   const response = await context.next();
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Max-Age', '86400');
-  response.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+  
+  // Get the origin of the request
+  const origin = context.request.headers.get('Origin');
+  
+  // List of allowed origins
+  const allowedOrigins = ['https://background.jlepis2010.workers.dev/'];
+  
+  if (origin && allowedOrigins.includes(origin)) {
+    response.headers.set('Access-Control-Allow-Origin', origin);
+    response.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+    response.headers.set('Access-Control-Max-Age', '86400');
+  }
+  
   return response;
 }
